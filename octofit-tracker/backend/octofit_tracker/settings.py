52850f9e3a6db786da671edf77f9969d184a34af
StephanNeese <s.neese@t-online.de>
2026-02-04 12:48:28 +0000

    Update settings and URLs to support Codespace deployment and improve API root response

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-_qor6t+$u(!m_pe!umfi6o6vo%7t*hv#ap+gzibez(=v&b5z0(
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
